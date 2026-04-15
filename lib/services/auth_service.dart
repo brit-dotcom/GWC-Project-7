1d5912336af259bd0393411f7eee322864004bf7
Hasita-K <kodehasita@gmail.com>
2026-04-14 20:44:37 -0400

    pushing firebase in the authservice

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,23 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<User?> signUp(String email, String password) async {
-    UserCredential result =
-        await _auth.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-    return result.user;
+    UserCredential result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    final user = result.user;
+
+    // Create the Firestore user document when a new account is made
+    if (user != null) {
+      await _db.collection('users').doc(user.uid).set({
+        'username': email.split('@')[0], // temp username from email
+        'coins': 0,                      // starting coin balance
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    }
+
+    return user;
   }
 
   Future<User?> signIn(String email, String password) async {
-    UserCredential result =
-        await _auth.signInWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
+    UserCredential result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
     return result.user;
   }
 
